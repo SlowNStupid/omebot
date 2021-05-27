0b92f14787893a3248d9caccdaf0bc25b2944f01
SlowNStupid <waranyoo.ronkainen@gmail.com>
2021-05-27 08:07:21 +0300

    -Refactored code

--- __main__.py
+++ __main__.py
@@ -14,7 +14,36 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 freq_users = {}
+food_pics = []
 nsfw_pics = []
+
+
+def get_members(message):
+    target_users = []
+    msg = message.content
+    match = re.search("\\s<@.(.+)>", msg)
+    if msg == "ome.sendfood":
+        target_users.append(message.author)
+    if match is not None:
+        mention_id = int(match.group(1))
+        target_user = message.guild.get_member(mention_id)
+        if target_user is not None:
+            target_users.append(target_user)
+        else:
+            target_role = message.guild.get_role(mention_id)
+            if target_role is not None:
+                for target_user in target_role.members:
+                    target_users.append(target_user)
+    return target_users
+
+
+async def send_picture_in_dm(target_users, author, message_to_others, message_to_self, picture):
+    for target_user in target_users:
+        dm_channel = await target_user.create_dm()
+        if target_user == author:
+            await dm_channel.send(content=message_to_self + picture.pop())
+        else:
+            await dm_channel.send(content=message_to_others + author.display_name + "\n" + picture.pop())
 
 
 @client.event
@@ -28,14 +57,46 @@ async def on_message(message):
             nsfw_pics = await reddit_scraper.get_nsfw_pics()
             random.shuffle(nsfw_pics)
 
+        global food_pics
+        if len(food_pics) < 1:
+            food_pics = await reddit_scraper.get_food_pics()
+            random.shuffle(food_pics)
+
         msg = message.content
         if msg == "ome":
             await message.channel.send("Olen Omena!")
         elif msg.startswith("ome.help"):
-            await message.channel.send("Commands:\n"
-                                       "    ome.clean\n"
-                                       "    ome.sendnude\n"
-                                       "    ome.nsfw\n")
+            cmd_embed = discord.Embed()
+            cmd_embed.description = "Commands:"
+            cmd_embed.add_field(name="ome.clean",
+                                value="Delete message bot has sent and also commands used to call bot "
+                                      "(if have permission) from the last 500 messages",
+                                inline=False)
+
+            cmd_embed.add_field(name="--------------------------------------------------------------------------------",
+                                value="------------------------------------------------------------------------------",
+                                inline=False)
+
+            cmd_embed.add_field(name="ome.food",
+                                value="Request bot to post food picture to the channel",
+                                inline=True)
+            cmd_embed.add_field(name="ome.sendfood <mention>",
+                                value="Request bot to send food porn to yourself or to other "
+                                      "(mention a person or role after the command)",
+                                inline=True)
+
+            cmd_embed.add_field(name="--------------------------------------------------------------------------------",
+                                value="------------------------------------------------------------------------------",
+                                inline=False)
+
+            cmd_embed.add_field(name="ome.nsfw",
+                                value="Request bot to post porn picture to the channel",
+                                inline=True)
+            cmd_embed.add_field(name="ome.sendnude <mention>",
+                                value="Request bot to send porn to yourself or to other "
+                                      "(mention a person or role after the command)",
+                                inline=True)
+            await message.channel.send(embed=cmd_embed)
         elif msg == "ome.clean":
             message_list = []
             async for cur_msg in message.channel.history(limit=200):
@@ -47,32 +108,33 @@ async def on_message(message):
                             await cur_msg.delete()
                     else:
                         message_list.append(cur_msg)
-
             if len(message_list) > 0:
                 await message.channel.delete_messages(message_list)
+        elif msg.startswith("ome.sendfood"):
+            target_users = []
+            if msg == "ome.sendfood":
+                target_users.append(message.author)
+            else:
+                target_users = get_members(message)
+            await send_picture_in_dm(target_users,
+                                     message.author,
+                                     "Food courtesy of ",
+                                     "Food courtesy of yourself??? Get a job, you lazy ass...\n",
+                                     food_pics)
+        elif msg == "ome.food":
+            await message.channel.send(content=food_pics.pop())
         elif message.channel.is_nsfw():
             if msg.startswith("ome.sendnude"):
-                match = re.search("ome.sendnude\\s<@.(.+)>", msg)
+                target_users = []
                 if msg == "ome.sendnude":
-                    dm_channel = await message.author.create_dm()
-                    await dm_channel.send(
-                        content="Nude courtesy of yourself??? You need help...\n" + nsfw_pics.pop())
-                    # await message.delete()
-                elif match is not None:
-                    mention_id = int(match.group(1))
-                    target_user = message.guild.get_member(mention_id)
-                    if target_user is not None:
-                        dm_channel = await target_user.create_dm()
-                        await dm_channel.send(
-                            content="Nude courtesy of " + message.author.display_name + "\n" + nsfw_pics.pop())
-                    else:
-                        target_role = message.guild.get_role(mention_id)
-                        if target_role is not None:
-                            for cur_member in target_role.members:
-                                dm_channel = await cur_member.create_dm()
-                                await dm_channel.send(
-                                    content="Nude courtesy of " + message.author.display_name + "\n" + nsfw_pics.pop())
-                    # await message.delete()
+                    target_users.append(message.author)
+                else:
+                    target_users = get_members(message)
+                    await send_picture_in_dm(target_users,
+                                         message.author,
+                                         "Nude courtesy of ",
+                                         "Nude courtesy of yourself??? You need some help...\n",
+                                         nsfw_pics)
             elif msg == "ome.nsfw":
                 await message.channel.send(content=nsfw_pics.pop())
 
@@ -82,14 +144,22 @@ async def on_message(message):
                 else:
                     freq_users[message.author.id] = freq_users[message.author.id] + 1
 
-                if random.randint(1, 10 + 1) >= 6:
+                if random.randint(1, 10 + 1) >= 9:
                     if freq_users[message.author.id] > 100:
                         await message.channel.send(
-                            content=message.author.mention + "... You need a help...")
+                            content=message.author.mention + "...You need help...")
+                    elif freq_users[message.author.id] > 75:
+                        await message.channel.send(
+                            content=message.author.mention + ", I see you're about to do a breakthrough in being dirty,"
+                                                             " requesting nudes over 75 times ;)")
                     elif freq_users[message.author.id] > 50:
                         await message.channel.send(
-                            content=message.author.mention + ", I see you're a real pervert,"
+                            content=message.author.mention + ", I see you're a real-deal pervert,"
                                                              " requesting nudes over 50 times ;)")
+                    elif freq_users[message.author.id] > 25:
+                        await message.channel.send(
+                            content=message.author.mention + ", I see you're not like any other perverts,"
+                                                             " requesting nudes over 25 times ;)")
                     elif freq_users[message.author.id] > 10:
                         await message.channel.send(
                             content=message.author.mention + ", I see you're a pervert,"
--- reddit_helper.py
+++ reddit_helper.py
@@ -4,6 +4,19 @@ import re
 import asyncpraw
 
 
+async def get_pictures_from_subreddit(subreddit):
+    result = []
+    async for submission in subreddit:
+        if submission.url.endswith(".jpg") \
+                or submission.url.endswith(".jpeg") \
+                or submission.url.endswith(".png") \
+                or submission.url.endswith(".webp") \
+                or submission.url.endswith(".gif") \
+                or submission.url.endswith(".gifv"):
+            result.append(submission.url)
+    return result
+
+
 class RedditScraper:
     def __init__(self, token_json: json):
         self.reddit = asyncpraw.Reddit(client_id=token_json["reddit_id"],
@@ -13,18 +26,9 @@ class RedditScraper:
                                        password=token_json["reddit_password"])
 
     async def get_nsfw_pics(self):
-        result = []
         nsfw_sub = (await self.reddit.subreddit("nsfw+NSFW_GIF")).hot(limit=500)
-        async for submission in nsfw_sub:
-            if submission.url.endswith(".jpg") \
-                    or submission.url.endswith(".jpeg") \
-                    or submission.url.endswith(".png") \
-                    or submission.url.endswith(".webp") \
-                    or submission.url.endswith(".gif"):
-                result.append(submission.url)
-            else:
-                match = re.search("redgifs.com/watch/(.+)", submission.url)
-                if match is not None:
-                    result.append(submission.url)
+        return await get_pictures_from_subreddit(nsfw_sub)
 
-        return result
+    async def get_food_pics(self):
+        food_porn_sub = (await self.reddit.subreddit("FoodPorn")).hot(limit=500)
+        return await get_pictures_from_subreddit(food_porn_sub)
